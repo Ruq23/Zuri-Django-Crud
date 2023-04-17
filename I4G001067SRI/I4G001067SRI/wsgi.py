abd991866a4acb2b5972440bd95622b47af88b6b
Faruq <98827527+Ruq23@users.noreply.github.com>
2023-04-17 13:28:27 +0100

    Update wsgi.py

--- I4G001067SRI/I4G001067SRI/wsgi.py
+++ I4G001067SRI/I4G001067SRI/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G001067SRI.settings')
 
 application = get_wsgi_application()
+
+app = application
